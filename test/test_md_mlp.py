001af68a2e120e8847003cc34417eb2681fe9a59
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2025-05-17 19:43:23 +0900

    Update test_md_mlp.py

--- test/test_md_mlp.py
+++ test/test_md_mlp.py
@@ -1,9 +1,16 @@
 from becmlp.mlp import run_mlp_md
+from becmlp.mlp.phonon import PhononCalculator
 
 
 def test_run_mlp_opt():
     cifname = "../input/FeSe_mp-20120_primitive.cif"
     run_mlp_md(cifname, nstep=150, press=0, temp=1000000000)
+    return
+
+def test_run_mlp_phonon():
+    cifname = "../input/FeSe_mp-20120_primitive.cif"
+    ph = PhononCalculator(cifname)
+    ph.calc_phonons()
     return
 
 if __name__ == "__main__":
